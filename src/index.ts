f965fc9ec08617282b24c61b4e11fc418cdc666c
Jackson Cummings <126146472+thelabcorner@users.noreply.github.com>
2026-08-29 10:58:58 -0500

    feat: add public package entrypoint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./core.js";
+export * from "./models.js";
+export * from "./observed-fetch.js";
+export * from "./research.js";
+export * from "./tracker.js";
+export * from "./types.js";
